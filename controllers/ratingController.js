ca390863a86a7dcde59f85613d3658bc606ff0cc
Amylouis <amylouis.brown@m.swu.ac.th>
2025-03-23 12:59:52 +0100

    22 : modified ratingPage.ejs and test page route

---
+++ controllers/ratingController.js
@@ -0,0 +1,4 @@
+exports.showRatingPage = (req, res) => {
+    res.render('ratingPage'); // ต้องมีไฟล์ views/ratingPage.ejs
+  };
+  
--- index.js
+++ index.js
@@ -3,15 +3,13 @@ const path = require('path');
 const session = require('express-session');
 const taskController = require('./controllers/taskController');
 const authController = require('./controllers/authController');
-
-
-
+const ratingController = require('./controllers/ratingController');
 // Initialize Express app
 const app = express();
+
 // Set EJS as the templating engine
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
-
 
 // Middleware to parse request body
 app.use(express.urlencoded({ extended: true }));
@@ -24,9 +22,9 @@ app.use(
     saveUninitialized: true,
   })
 );
-// Routes
 
-app.get('/', authController.authenticate, taskController.getTasks);
+// Routes
+app.get('/', ratingController.showRatingPage);
 app.get('/view/:name', taskController.viewTask);
 app.get('/logout', authController.logout);
 app.get('/sort', authController.authenticate, taskController.sortTasksByPriority);
@@ -35,7 +33,7 @@ app.post('/login', authController.login);
 app.post('/add', authController.authenticate, taskController.addTask);
 app.post('/delete', authController.authenticate, taskController.deleteMultipleTasks);
 app.post('/search', authController.authenticate, taskController.searchTasksByName);
-
+app.get('/ratingPage', ratingController.showRatingPage); 
 
 // Start server
 const PORT = process.env.PORT || 3000;
